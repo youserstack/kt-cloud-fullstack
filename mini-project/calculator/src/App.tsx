a25cd85ae4d746b96ce98601e229eb95f10173a8
youserstack <youserstack@gmail.com>
2025-10-22 19:52:20 +0900

    update

--- mini-project/calculator/src/App.tsx
+++ mini-project/calculator/src/App.tsx
@@ -3,14 +3,15 @@ import Calc from "./pages/Calc";
 import Layout from "./pages/Layout";
 import Login from "./pages/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { HashRouter, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./store/auth";
 
 export default function App() {
   return (
     <div className="App">
       <AuthProvider>
-        <HashRouter>
+        <BrowserRouter>
           <Routes>
             <Route element={<Layout />}>
               {/* public */}
@@ -23,7 +24,7 @@ export default function App() {
               </Route>
             </Route>
           </Routes>
-        </HashRouter>
+        </BrowserRouter>
       </AuthProvider>
     </div>
   );
