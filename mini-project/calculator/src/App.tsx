a1e899feb7bfb0968f5bd45b833812dd695eb0bc
youserstack <youserstack@gmail.com>
2025-10-23 09:13:16 +0900

    update

--- mini-project/calculator/src/App.tsx
+++ mini-project/calculator/src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <div className="App">
       <AuthProvider>
-        <BrowserRouter basename="/kt-cloud-fullstack">
+        <BrowserRouter basename="/kt-cloud-fullstack/mini-project/calculator">
           <Routes>
             <Route element={<Layout />}>
               {/* Public Routes */}
